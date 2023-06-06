3b208de6f051e74356518f07e05fb70ef5cc9c06
Ng Sheng Tat <shengtatng@gmail.com>
2023-06-06 18:39:25 +0800

    test_again

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from PIL import Image
 
 from streamlit_image_coordinates import streamlit_image_coordinates
 image = Image.open("Image4.png")
+image = image.convert("RGB")
 value = streamlit_image_coordinates(image)
 
 st.write(value)
