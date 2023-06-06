30a74054da5190709c866c84f66131c0985163b3
Ng Sheng Tat <shengtatng@gmail.com>
2023-06-06 18:36:05 +0800

    read_some_other_image

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ from PIL import Image
 
 from streamlit_image_coordinates import streamlit_image_coordinates
 image = Image.open("Image4.png")
-value = streamlit_image_coordinates("https://placekitten.com/200/300")
+value = streamlit_image_coordinates(image)
 
 st.write(value)
