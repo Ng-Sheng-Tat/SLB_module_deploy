6a3e8342207071f6e3ec8b2397f445a583a307a6
Ng Sheng Tat <shengtatng@gmail.com>
2023-06-06 18:24:45 +0800

    new images

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import streamlit as st
+from PIL import Image
 
 from streamlit_image_coordinates import streamlit_image_coordinates
-
+image = Image.open("Image4.png")
 value = streamlit_image_coordinates("https://placekitten.com/200/300")
 
 st.write(value)
